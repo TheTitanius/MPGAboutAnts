086cc84bce199e90bdb33b8278454092b5d4ce4d
Александр Мигас <migas.alexandr@gmail.com>
2023-08-31 01:50:01 +0300

    Реализовал эллиптическую карту

--- MPGAboutAnts/wwwroot/js/hexScripts.js
+++ MPGAboutAnts/wwwroot/js/hexScripts.js
@@ -1,16 +1,28 @@
-﻿function AddHex(column = 10, line = 10) {
+﻿function AddHex(column = 25, line = 18) {
     map = document.getElementById("map");
 
     for (let i = 0; i < column; i++) {
         const col = document.createElement('div');
-        if (i % 2 == 0) {
+        if (i % 2 != 0) {
             col.classList.add('honest-column');
         } else {
             col.classList.add('odd-column');
         }
         for (let j = 0; j < line; j++) {
             var div = document.createElement("div");
-            div.className = "hex";
+            if (((i == 0 || i == 24) && (j < 7 || j > 10)) ||
+                ((i == 1 || i == 23) && (j < 5 || j > 11)) ||
+                ((i == 2 || i == 22) && (j < 4 || j > 13)) ||
+                ((i == 3 || i == 21) && (j < 2 || j > 14)) ||
+                ((i == 4 || i == 20) && (j < 2 || j > 15)) ||
+                ((i == 5 || i == 19) && (j < 1 || j > 15)) ||
+                ((i == 6 || i == 18) && (j < 1 || j > 16)) ||
+                ((i == 7 || i == 9 || i == 11 || i == 13 || i == 15 || i == 17) && (j > 16))
+            ) {
+                div.className = "void-hex";
+            } else {
+                div.className = "hex";
+            }
             div.id = `${i}, ${j}`;
             col.appendChild(div);
         }
