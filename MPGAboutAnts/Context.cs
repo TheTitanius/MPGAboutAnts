ef938de78e0387f93146b88498a1800b9eb0f76d
Александр Мигас <migas.alexandr@gmail.com>
2023-08-31 18:58:37 +0300

    Добавил базу данных

---
+++ MPGAboutAnts/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MPGAboutAnts.Models;
+
+namespace MPGAboutAnts
+{
+    public class Context : DbContext
+	{
+		public Context(DbContextOptions<Context> options) : base(options)
+		{
+			//Database.EnsureDeleted();
+			//Database.EnsureCreated();
+		}
+
+		public DbSet<HexType> HexTypes { get; set; }
+	}
+}
---
+++ MPGAboutAnts/Models/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace MPGAboutAnts.Models
+{
+	public interface IEntity
+	{
+		int Id { get; }
+	}
+}
