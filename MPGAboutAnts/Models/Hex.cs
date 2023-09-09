d75ffbde4f3f3af2547cacdf3afdb54a27b5101b
Александр Мигас <migas.alexandr@gmail.com>
2023-09-09 12:57:23 +0300

    Добавил модель гекса

--- MPGAboutAnts/Models/Hex.cs
+++ MPGAboutAnts/Models/Hex.cs
@@ -5,7 +5,7 @@
 		public int Id { get; set; }
 		public int X { get; set; }
 		public int Y { get; set; }
-		public HexType HexType { get; set; }
+		public HexType? HexType { get; set; }
 		public Map Map { get; set; }
 		public Unit? Unit { get; set; }
 	}
