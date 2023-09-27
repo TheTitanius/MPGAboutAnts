52aba52b2935c8ebe9d13d7d716061b69703e28e
Александр Мигас <migas.alexandr@gmail.com>
2023-09-27 14:41:28 +0300

    Добавил глобальные переменные состояния

--- MPGAboutAnts/wwwroot/js/site.js
+++ MPGAboutAnts/wwwroot/js/site.js
@@ -1,6 +1,16 @@
 ﻿var gameMode = true;
 var isSetHex = true;
+var isSetAnt = true;
+
+var antType = "ant";
 var hexType = "ground";
+
+var playerColor = "#000000"
+var playerId = 0;
+
+var ants = [];
+
+let antId = 0;
 
 async function createModel(modelsName, newModel) {
     const response = await fetch(`/api/${modelsName}`, {
