f50e6cd78496d54602035d590d217be4d283ca02
Александр Мигас <migas.alexandr@gmail.com>
2023-09-27 14:11:56 +0300

    Добавил скрипт для работы с муравьями

---
+++ MPGAboutAnts/wwwroot/js/antScript.js
@@ -0,0 +1,36 @@
+﻿class Ant {
+    id;
+    type;
+    hp;
+    damage;
+    x;
+    y;
+    playerId;
+    color;
+
+    constructor(id, type, playerId, color) {
+        this.id = id;
+        this.type = type;
+        switch (type) {
+            case "ant":
+                this.hp = 100;
+                this.damage = 10;
+                break;
+            case "warrior":
+                this.hp = 80;
+                this.damage = 20;
+                break;
+            default:
+                this.hp = 100;
+                this.damage = 10;
+                break;
+        }
+        this.playerId = playerId;
+        this.color = color;
+    }
+
+    setXY(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
