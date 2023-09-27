447877e334bb65d9e3997768f596b52ca5df72c8
Александр Мигас <migas.alexandr@gmail.com>
2023-09-27 14:22:03 +0300

    Добавил к модели юнита здоровье и урон

--- MPGAboutAnts/Models/Unit.cs
+++ MPGAboutAnts/Models/Unit.cs
@@ -4,8 +4,10 @@
 	{
 		public int Id { get; set; }
 		public UnitType Type { get; set; }
+		public Player Player { get; set; }
 		public Hex Hex { get; set; }
-
+		public int Hp { get; set; }
+		public int Damage { get; set; }
 		public int HexId { get; set; }
 	}
 }
